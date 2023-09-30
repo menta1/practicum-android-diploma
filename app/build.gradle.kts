5014508b25a56d8c33305e8001bb78a292b2cbc0
Menta1 <Bulatov1993>
2023-10-01 00:54:45 +0500

    GH-1:Убрал лишнее

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,6 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.7.2")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
