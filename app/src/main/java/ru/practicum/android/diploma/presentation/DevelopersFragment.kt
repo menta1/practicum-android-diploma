95580237dbf72f6823c04fa9d41132a773dc3f6f
alexxk2 <alexx.koda@gmail.com>
2023-10-01 01:51:57 +0300

    GH-3-fragments-and-layouts: Созданы фрагменты и лэйауты

---
+++ app/src/main/java/ru/practicum/android/diploma/presentation/DevelopersFragment.kt
@@ -0,0 +1,41 @@
+package ru.practicum.android.diploma.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.databinding.FragmentDevelopersBinding
+
+
+class DevelopersFragment : Fragment() {
+
+    private var _binding: FragmentDevelopersBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentDevelopersBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
