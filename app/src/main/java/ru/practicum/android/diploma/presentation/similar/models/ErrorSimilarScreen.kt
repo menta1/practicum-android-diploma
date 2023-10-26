dc8fd2577d86e919b5f1f07e86e03467a68a0dd8
ImNia <agnia-kimi1996@mail.ru>
2023-10-26 22:50:33 +0700

    Added toast when error occurred while loading data

---
+++ app/src/main/java/ru/practicum/android/diploma/presentation/similar/models/ErrorSimilarScreen.kt
@@ -0,0 +1,8 @@
+package ru.practicum.android.diploma.presentation.similar.models
+
+enum class ErrorSimilarScreen {
+    NOT_INTERNET,
+    ERROR,
+    NOT_INTERNET_FOR_LOADING_DATA,
+    ERROR_WITH_LOADING_DATA
+}
--- app/src/main/java/ru/practicum/android/diploma/presentation/similar/models/SimilarState.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/similar/models/SimilarState.kt
@@ -3,11 +3,12 @@ package ru.practicum.android.diploma.presentation.similar.models
 import ru.practicum.android.diploma.domain.models.Vacancy
 
 sealed interface SimilarState {
-    object Loading: SimilarState
+    data object Loading: SimilarState
     data class Content(
         val data: List<Vacancy>
     ): SimilarState
-    object Empty: SimilarState
-    object Error: SimilarState
-    object NoInternet: SimilarState
+    data object Empty: SimilarState
+    data class Error(
+        val error: ErrorSimilarScreen
+    ): SimilarState
 }
