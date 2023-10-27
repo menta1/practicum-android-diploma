2fbb7815b629053cba95a57daa21f7be793e378c
alexxk2 <alexx.koda@gmail.com>
2023-10-27 15:46:17 +0300

    GH-179: фикс когда не ищет после кнопки Сбросить

--- app/src/main/java/ru/practicum/android/diploma/presentation/filter/models/FilterScreenState.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/filter/models/FilterScreenState.kt
@@ -2,13 +2,14 @@ package ru.practicum.android.diploma.presentation.filter.models
 
 sealed interface FilterScreenState{
 
-    object Default: FilterScreenState
+    data class Default(val isClearButtonPressed: Boolean = false): FilterScreenState
     data class Content(
         val countryName: String? = null,
         val regionName: String? = null,
         val industryName: String? = null,
         val expectedSalary: Long? = null,
-        val isOnlyWithSalary: Boolean = false
+        val isOnlyWithSalary: Boolean = false,
+        val isClearButtonPressed: Boolean = false
     ): FilterScreenState
     data class SalaryInput(val isInputNotEmpty: Boolean): FilterScreenState
 
